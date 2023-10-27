f26d7ecde3ec225ec3ab2f3fdeaeaaddf8671be4
Shilpa <reddy.shilparaj@gmail.com>
2023-10-27 14:19:23 +0100

    changed values

--- src/test/java/CucumberFramework/TestRunn.java
+++ src/test/java/CucumberFramework/TestRunn.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/java/CucumberFramework",glue="StepDefinition",
-monochrome=true,tags="@error",plugin= {"html:target/cucumber.html"})
+monochrome=true,tags="@error",plugin= {"html:target/cucumbers.html"})
 public class TestRunn extends AbstractTestNGCucumberTests{
 	
 	
